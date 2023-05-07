3e85c05f33a386c430fbb2acfa4c003621feec4d
dudava <Dudavik356@yandex.ru>
2023-05-07 17:17:07 +0600

    small db fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_restful import Api
 
 from user_resources import UserListResource
-from db import db_init
+from db import db_init, con
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -17,5 +17,6 @@ def index():
 
 
 if __name__ == '__main__':
-	db_init()
+	db_init('db.sqlite')
+	print(con)
 	app.run('127.0.0.1', 5000)
